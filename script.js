d75bbb3b3bd34b7835399251d79b752e8f9218ab
kadraknb <kadrak02@hotmail.com>
2022-04-23 09:53:44 -0300

    Requisitos 5

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ for (let i = 0; i < 5; i += 1) {
   for (let i2 = 0; i2 < 5; i2 += 1) {
     document.querySelectorAll('.colunaPixel')[i2]
       .appendChild(document.createElement('div'))
-      .className = ' color2 white pixel';
+      .className = 'pixel';
   }
 }
