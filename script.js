3f7bdc19f02c7ac075c1de1e4f97035529ec4fab
kadraknb <kadrak02@hotmail.com>
2022-04-25 19:12:44 -0300

    Requisitos 8

--- script.js
+++ script.js
@@ -3,45 +3,68 @@ for (let i3 = 0; i3 < 5; i3 += 1) {
     .appendChild(document.createElement('div'))
     .className = 'colunaPixel';
 }
-function aleta() {
-  document.querySelector('.selected').classList.remove('selected');
-  document.getElementsByClassName('color')[1].classList.add('selected');
-}
+
 for (let i = 0; i < 5; i += 1) {
   for (let i2 = 0; i2 < 5; i2 += 1) {
     document.querySelectorAll('.colunaPixel')[i2]
       .appendChild(document.createElement('div'))
-      .className = 'pixel';
+      .className = 'pixel futuracor';
   }
 }
+//window.onload
 
 const color1 = document.getElementsByClassName('color');
-
+const tabeladecor = document.querySelector('#pixel-board');
 
 
 
 color1[0].addEventListener("click", function() {
   document.querySelector('.selected').classList.remove('selected');
-    document.getElementsByClassName('color')[0].classList.add('selected');
+    document.getElementsByClassName('color')[0].classList.add('selected');console.log(event.target.className);
   });
   color1[1].addEventListener("click", function() {
   document.querySelector('.selected').classList.remove('selected');
-    document.getElementsByClassName('color')[1].classList.add('selected');
+    document.getElementsByClassName('color')[1].classList.add('selected');console.log(event.target.className);
   });
   color1[2].addEventListener("click", function() {
   document.querySelector('.selected').classList.remove('selected');
-    document.getElementsByClassName('color')[2].classList.add('selected');
+    document.getElementsByClassName('color')[2].classList.add('selected');console.log(event.target.className);
   });
   color1[3].addEventListener("click", function() {
   document.querySelector('.selected').classList.remove('selected');
     document.getElementsByClassName('color')[3].classList.add('selected');
+    console.log(event.target.className);
   });
 
+  tabeladecor.addEventListener("click", function() {
+    let cor = document.querySelector('.selected').classList[1];
+    let localPinta = event.target;
+    let removertinta = localPinta.classList[1]
+    localPinta.classList.remove(removertinta);
+    localPinta.classList.add(cor);
+    console.log(event.target);
+})
+    //let localPinta = event.target.classList
+/* 
+//localPinta.className += " " + cor;
 
 
-/* function ondeclick() {
+
+//localPinta.classList[1].remove(function(){})
+/* localPinta.classList[1].add(function(){
+      document.querySelector('.selected').classList[1];
+      console.log(event.target);
+    }); 
+function ondeclick() {
   color3
 
+  tabeladecor.addEventListener("click", () => {
+    let cor = document.querySelector('.selected').classList[1];
+    let localPinta = event.target;
+    localPinta.classList[1].remove
+    localPinta.className = " " + cor;
+    console.log(event.target);
+})
 
 }
  */
