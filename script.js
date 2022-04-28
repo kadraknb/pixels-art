861c7df8506da8b48f04e1ce5a26a5fe331961b9
kadraknb <kadrak02@hotmail.com>
2022-04-27 21:28:51 -0300

    Requisitos 9

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+let botaoXY = document.getElementById('generate-board');
+
+
+
+
 for (let i3 = 0; i3 < 5; i3 += 1) {
-  document.querySelector('#pixel-board')
+  document.querySelector('#pixel-board2')
     .appendChild(document.createElement('div'))
     .className = 'colunaPixel';
 }
@@ -11,41 +16,107 @@ for (let i = 0; i < 5; i += 1) {
       .className = 'pixel futuracor';
   }
 }
-//window.onload
+
+
+botaoXY.addEventListener("click", function () {
+  let xy = document.getElementById('board-size').value;
+  if (document.getElementById('board-size').value > 50 
+  || document.getElementById('board-size').value < 5 
+  || document.getElementById('board-size').value == "") {
+    alert('Board inválido!');
+    return
+  }
+
+  document.querySelector('#pixel-board').removeChild(document.querySelector('#pixel-board2'));
+
+
+  for (let i3 = 0; i3 < xy; i3 += 1) {
+    document.querySelector('#pixel-board')
+      .appendChild(document.createElement('div'))
+      .className = 'colunaPixel';
+  }
+  for (let i = 0; i < xy; i += 1) {
+    for (let i2 = 0; i2 < xy; i2 += 1) {
+      document.querySelectorAll('.colunaPixel')[i2]
+        .appendChild(document.createElement('div'))
+        .className = 'pixel futuracor';
+    }
+  }
+  console.log(xy);
+
+});
+
+
+/* function quantidadeDePixel() {
+  for (let i3 = 0; i3 < xy; i3 += 1) {
+    document.querySelector('#pixel-board')
+      .appendChild(document.createElement('div'))
+      .className = 'colunaPixel';
+  }
+
+  for (let i = 0; i < xy; i += 1) {
+    for (let i2 = 0; i2 < xy; i2 += 1) {
+      document.querySelectorAll('.colunaPixel')[i2]
+        .appendChild(document.createElement('div'))
+        .className = 'pixel futuracor';
+    }
+  } console.log(xy);
+} */
+
+//window.onload = quantidadeDePixel()
 
 const color1 = document.getElementsByClassName('color');
 const tabeladecor = document.querySelector('#pixel-board');
 
 
 
-color1[0].addEventListener("click", function() {
+color1[0].addEventListener("click", function () {
   document.querySelector('.selected').classList.remove('selected');
-    document.getElementsByClassName('color')[0].classList.add('selected');console.log(event.target.className);
-  });
-  color1[1].addEventListener("click", function() {
+  document.getElementsByClassName('color')[0].classList.add('selected'); console.log(event.target.className);
+});
+color1[1].addEventListener("click", function () {
   document.querySelector('.selected').classList.remove('selected');
-    document.getElementsByClassName('color')[1].classList.add('selected');console.log(event.target.className);
-  });
-  color1[2].addEventListener("click", function() {
+  document.getElementsByClassName('color')[1].classList.add('selected'); console.log(event.target.className);
+});
+color1[2].addEventListener("click", function () {
   document.querySelector('.selected').classList.remove('selected');
-    document.getElementsByClassName('color')[2].classList.add('selected');console.log(event.target.className);
-  });
-  color1[3].addEventListener("click", function() {
+  document.getElementsByClassName('color')[2].classList.add('selected'); console.log(event.target.className);
+});
+color1[3].addEventListener("click", function () {
   document.querySelector('.selected').classList.remove('selected');
-    document.getElementsByClassName('color')[3].classList.add('selected');
-    console.log(event.target.className);
-  });
+  document.getElementsByClassName('color')[3].classList.add('selected');
+  console.log(event.target.className);
+});
 
-  tabeladecor.addEventListener("click", function() {
-    let cor = document.querySelector('.selected').classList[1];
-    let localPinta = event.target;
-    let removertinta = localPinta.classList[1]
-    localPinta.classList.remove(removertinta);
-    localPinta.classList.add(cor);
-    console.log(event.target);
+tabeladecor.addEventListener("click", function () {
+  let cor = document.querySelector('.selected').classList[1];
+  let localPinta = event.target;
+  let removertinta = localPinta.classList[1]
+  localPinta.classList.remove(removertinta);
+  localPinta.classList.add(cor);
+  console.log(event.target);
 })
+let limpar = document.querySelector('#clear-board');
+limpar.addEventListener("click", function () {
+  location.reload();
+});
+
+//clear-board
+
+/* limpar.addEventListener("click", function() {
+  for (let i = 0; i < 5; i += 1) {
+    for (let i2 = 0; i2 < 5; i2 += 1) {
+      document.querySelectorAll('.colunaPixel')[i2]
+        .appendChild.className = 'pixel futuracor';
+    }
+  }
+}); */
+
+
+
+
     //let localPinta = event.target.classList
-/* 
+/*
 //localPinta.className += " " + cor;
 
 
@@ -54,7 +125,7 @@ color1[0].addEventListener("click", function() {
 /* localPinta.classList[1].add(function(){
       document.querySelector('.selected').classList[1];
       console.log(event.target);
-    }); 
+    });
 function ondeclick() {
   color3
 
